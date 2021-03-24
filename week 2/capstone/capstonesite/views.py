c8a843fbc699ac672d093315caef292fc6c0279a
Brev860 <brevan.daniels@bryanuniversity.edu>
2021-03-23 21:05:10 -0400

    week2

---
+++ week 2/capstone/capstonesite/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegistrationForm
+from .models import *
+
+
+
+# Create your views here.
+
+
+def registration(response):
+    form = UserCreationForm(response.POST)
+
+    if response.method == 'POST':
+        if form.is_valid():
+            form.save()
+       
+    return render(response, 'registration/registration.html', {'form': form})
+        
+    
+
