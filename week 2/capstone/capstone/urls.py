6e953b33b339f98b5efc0e38d66d62af733ecb15
Brev860 <brevan.daniels@bryanuniversity.edu>
2021-04-04 15:40:27 -0400

    registration/login

--- week 2/capstone/capstone/urls.py
+++ week 2/capstone/capstone/urls.py
@@ -22,8 +22,8 @@ from django.conf.urls.static import static
 from capstonesite.views import home
 urlpatterns = [
     path('', include('capstonesite.urls')),
-    path('admin/', admin.site.urls),
-    path('',home)
+    path('admin', admin.site.urls),
+    
     
   
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- week 2/capstone/capstonesite/views.py
+++ week 2/capstone/capstonesite/views.py
@@ -4,21 +4,39 @@ from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
 from .forms import RegistrationForm
 from .models import *
-
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
 
 
-def registration(response):
-    form = UserCreationForm(response.POST)
+def registration(request):
+    form = UserCreationForm()
 
-    if response.method == 'POST':
+    if request.method == 'POST': 
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-       
-    return render(response, 'registration/registration.html', {'form': form})
+            return redirect('login')
+            
+    return render(request, 'registration/registration.html', {'form': form})
 
 def home(request):
     return render(request, 'home.html')
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+       
+        user = authenticate(username = username , password = password)
+        
+        if user is not None:
+           
+            login(request, user)
+            return redirect('home')
+    form = {}
+    return render(request, 'registration/login.html', form)
    
